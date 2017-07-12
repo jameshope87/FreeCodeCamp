cf1c400cb6f2769bdbcf5945a82bcbf2e059f974
James Hope <james.hope87@gmail.com>
2017-07-13 00:45:51 +0100

    Added template for portfolio page

---
+++ Portfolio/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+
+// Highlight the active nav item
+$(".nav a").on("click", function(){
+   $(".nav").find(".active").removeClass("active");
+   $(this).parent().addClass("active");
+});
+
+$('body').scrollspy({target: '.navbar-fixed-top'})
+
+// JQuery for scrolling page links
+$(function() {$('body').on('click', 'a.scrollable', function(event) {
+  var $anchor = $(this);
+  $('html, body').stop().animate({scrollTop: $($anchor.attr('href')).offset().top},1500,'easeInOutExpo');
+  event.preventDefault();
+  });
+});
+
+});
