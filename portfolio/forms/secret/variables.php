851d22c7fdcfc3eee42b2c0950df7709f28d6998
James Hope <james.hope87@gmail.com>
2017-07-14 10:55:56 +0100

    Changed directory structure

--- portfolio/forms/secret/variables.php
+++
@@ -1,9 +0,0 @@
-<?php
-$secretKey = "6Ldj5SgUAAAAAAVlYJq8OHmR1B12WZkq-Vsy_yER";
-?>
-
-if(mail($to, $subject, $body, $from)) {
-                $result='<div class="alert alert-success">Thank you! I will be in touch</div>';
-            } else {
-                $result='<div class="alert alert-danger">Sorry there was an error sending your message. Please try again later</div>';
-            }
